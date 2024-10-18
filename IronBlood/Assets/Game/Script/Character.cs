93d2aca2ec0f44feb7eb604ea2f94052c4d60d68
emrekocadere <emrekocadere34@gmail.com>
2024-10-18 22:38:29 +0300

    feat:add Character

---
+++ IronBlood/Assets/Game/Script/Character.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+public class Character : MonoBehaviour
+{
+   private CharacterController _cc;
+    public float MoveSpeed = 5f;
+    private Vector3 _movementVelocity;
+    private PlayerInput _playerInput;
+    private float _verticalVelocity;
+    public float Gravity = -9.8f;
+    private Animator _animator;
+
+    public int Coin;
+
+    //Enemy
+    public bool IsPlayer = true;
+    private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    private Transform TargetPlayer;
+
+    //Health
+    private Health _health;
+
+    //Damage Caster
+    private DamageCaster _damageCaster;
+
+    //Player Slides
+    private float attackStartTime;
+    public float AttackSlideDuration = 0.4f;
+    public float AttackSlideSpeed = 0.06f;
+
+    private Vector3 impactOnCharacter;
+
+    public bool IsInvincible;
+    public float invincibleDuration = 2f;
+
+    private float attackAnimationDuration;
+    public float SlideSpeed = 9f;
+
+    //State Machine
+    public enum CharacterState
+    {
+        Normal,Attacking,Dead,BeingHit,Slide,Spawn
+    }
+    public CharacterState CurrentState;
+    public float SpawnDuration = 2f;
+    private float currentSpawnTime;
+
+    //Material Animation
+    private MaterialPropertyBlock _materialPropertyBlock;
+    private SkinnedMeshRenderer _skinnedMeshRenderer;
+    public GameObject ItemToDrop;
+
+    private void Awake()
+    {
+        _cc = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+        _health = GetComponent<Health>();
+        _damageCaster = GetComponentInChildren<DamageCaster>();
+
+        _skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        _materialPropertyBlock = new MaterialPropertyBlock();
+        _skinnedMeshRenderer.GetPropertyBlock(_materialPropertyBlock);
+
+        if (!IsPlayer)
+        {
+            _navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            TargetPlayer = GameObject.FindWithTag("Player").transform;
+            _navMeshAgent.speed = MoveSpeed;
+            SwitchStateTo(CharacterState.Spawn);
+        }
+        else
+        {
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+    }
+
+
+
+}
